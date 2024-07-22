8e653ab015a20ed54de14c3439f5f27519fe7ebd
Tomasz Wiszkowski <tomasz.wiszkowski@gmail.com>
2024-07-21 22:28:06 -0700

    Add GerritChangeIdAction tests
    Change-Id: I069c7b7d347775edf328995390ccebe823af3585

--- src/hooks/gerrit.rs
+++ src/hooks/gerrit.rs
@@ -3,7 +3,7 @@ use anyhow::Context;
 use anyhow::Result;
 
 use chrono::Local;
-use git2::Config;
+
 use git2::Repository;
 use log::info;
 use log::warn;
@@ -18,7 +18,7 @@ use crate::repo::GitConfig;
 use super::action::ActionTraitInternal;
 use super::ActionTrait;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct GerritChangeIdAction {
     #[serde(skip_deserializing)]
@@ -55,12 +55,12 @@ impl GerritChangeIdAction {
         hasher.to_string()
     }
 
-    fn should_generate_change_id(config: &Config, commit_msg: &str) -> bool {
-        match config
-            .get_string("gerrit.createChangeId")
-            .unwrap_or_default()
-            .as_str()
-        {
+    pub fn should_generate_change_id(
+        requested_type: &str,
+        commit_msg: &str,
+        trailers: &BTreeMap<String, String>,
+    ) -> bool {
+        match requested_type {
             "false" => {
                 info!("Skipping Change-Id creation: gerrit.createChangeId set to `false'");
                 false
@@ -74,12 +74,15 @@ impl GerritChangeIdAction {
             _ => {
                 // Do not create a change id for squash/fixup commits.
                 let first_line = commit_msg.lines().next().unwrap_or_default();
-                let generate_change_id =
-                    !Regex::new(r"^[a-z][a-z]*! ").unwrap().is_match(first_line);
-                if !generate_change_id {
-                    info!("Skipping squash/fixup commit: {}", first_line);
+                let mut skip_tag_creation =
+                    Regex::new(r"^[a-z][a-z]*! ").unwrap().is_match(first_line);
+                if !skip_tag_creation {
+                    info!("Skipping possible squash/fixup commit: {}", first_line);
                 }
-                generate_change_id
+
+                skip_tag_creation |=
+                    trailers.contains_key(TAG_CHANGE_ID) || trailers.contains_key(TAG_CHANGE_LINK);
+                !skip_tag_creation
             }
         }
     }
@@ -96,24 +99,30 @@ impl GerritChangeIdAction {
         let repo = Repository::open(".").expect("Failed to open repository");
         let config = repo.config().expect("Failed to get config");
 
-        if !Self::should_generate_change_id(&config, &commit_msg) {
+        if !Self::should_generate_change_id(
+            &config
+                .get_string("gerrit.createChangeId")
+                .unwrap_or_default(),
+            &commit_msg,
+            &trailers,
+        ) {
             return Ok(());
         }
 
         if let Some(review_url) = config.get_string("gerrit.reviewUrl").ok() {
-            info!("Checking for `{}'", TAG_CHANGE_LINK);
-            trailers.entry(TAG_CHANGE_LINK.into()).or_insert_with(|| {
+            trailers.insert(
+                TAG_CHANGE_LINK.into(),
                 format!(
                     "{}/id/I{}",
                     review_url.trim_end_matches('/'),
                     Self::generate_hash(&repo)
-                )
-            });
+                ),
+            );
         } else {
-            info!("Checking for `{}'", TAG_CHANGE_ID);
-            trailers
-                .entry(TAG_CHANGE_ID.into())
-                .or_insert_with(|| format!("I{}", Self::generate_hash(&repo)));
+            trailers.insert(
+                TAG_CHANGE_ID.into(),
+                format!("I{}", Self::generate_hash(&repo)),
+            );
         };
 
         Ok(fs::write(
@@ -136,7 +145,7 @@ impl GerritChangeIdAction {
         }
 
         // Skip empty lines at end...
-        while lines.len() > 2 {
+        while lines.len() > 1 {
             if !lines.last().unwrap().is_empty() {
                 break;
             }
@@ -147,17 +156,19 @@ impl GerritChangeIdAction {
         let mut trailers = BTreeMap::new();
         let trailer_regex = Regex::new(r"^([a-zA-Z0-9-]+):\s*(.*)\s*$").unwrap();
 
-        while lines.len() > 2 {
+        while lines.len() > 1 {
             let line = lines.last().unwrap();
             if let Some(caps) = trailer_regex.captures(line) {
                 info!("Found trailer {}: {}", &caps[1], &caps[2]);
                 trailers.insert(caps[1].to_string(), caps[2].to_string());
                 lines.pop();
+            } else {
+                break;
             }
         }
 
         // ... then, skip empty lines at end.
-        while lines.len() > 2 {
+        while lines.len() > 1 {
             if !lines.last().unwrap().is_empty() {
                 break;
             }
@@ -171,7 +182,7 @@ impl GerritChangeIdAction {
     fn compile_commit_message(message: &str, trailers: &BTreeMap<String, String>) -> String {
         let mut res = message.to_owned();
 
-        res += "\n";
+        res += "\n\n";
         for (key, val) in trailers {
             res += format!("{}: {}\n", key, val).as_str();
         }
@@ -225,5 +236,149 @@ impl ActionTrait for GerritChangeIdAction {
 
     fn run(&self, _files: &[String], args: &Vec<String>) -> Result<()> {
         Self::generate_change_id(&args[0])
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_should_generate_change_id_never() {
+        assert_eq!(
+            false,
+            GerritChangeIdAction::should_generate_change_id(
+                "false",
+                "Commit Message",
+                &BTreeMap::new()
+            )
+        );
+    }
+
+    #[test]
+    fn test_should_generate_change_id_always() {
+        assert_eq!(
+            true,
+            GerritChangeIdAction::should_generate_change_id(
+                "always",
+                "Commit Message",
+                &BTreeMap::new()
+            )
+        );
+    }
+
+    #[test]
+    fn test_should_generate_change_id_needed() {
+        let mut map = BTreeMap::new();
+        map.insert("ChangeId".to_owned(), "1234".to_owned());
+        assert_eq!(
+            true,
+            GerritChangeIdAction::should_generate_change_id("true", "Commit Message", &map)
+        );
+    }
+
+    #[test]
+    fn test_should_generate_change_id_not_needed_fixup() {
+        assert_eq!(
+            false,
+            GerritChangeIdAction::should_generate_change_id(
+                "true",
+                "fixup! Commit Message",
+                &BTreeMap::new()
+            )
+        );
+    }
+
+    #[test]
+    fn test_should_generate_change_id_not_needed_change_id_exists() {
+        let mut map = BTreeMap::new();
+        map.insert(TAG_CHANGE_ID.to_owned(), "1234".to_owned());
+        assert_eq!(
+            false,
+            GerritChangeIdAction::should_generate_change_id("true", "Commit Message", &map)
+        );
+    }
+
+    #[test]
+    fn test_should_generate_change_id_not_needed_link_exists() {
+        let mut map = BTreeMap::new();
+        map.insert(TAG_CHANGE_LINK.to_owned(), "1234".to_owned());
+        assert_eq!(
+            false,
+            GerritChangeIdAction::should_generate_change_id("true", "Commit Message", &map)
+        );
+    }
+
+    #[test]
+    fn test_parse_commit_msg_single_line() {
+        let (message, trailers) =
+            GerritChangeIdAction::parse_commit_message("This is a message").unwrap();
+        assert_eq!("This is a message", message);
+        assert_eq!(0, trailers.len());
+    }
+
+    #[test]
+    fn test_parse_commit_msg_single_line_with_newlines() {
+        let (message, trailers) =
+            GerritChangeIdAction::parse_commit_message("This is a message\n\n\n\n").unwrap();
+        assert_eq!("This is a message", message);
+        assert_eq!(0, trailers.len());
+    }
+
+    #[test]
+    fn test_parse_commit_msg_single_line_with_multilines() {
+        let (message, trailers) = GerritChangeIdAction::parse_commit_message(
+            "This is a message\n\nThis is a description\n\n",
+        )
+        .unwrap();
+        assert_eq!("This is a message\n\nThis is a description", message);
+        assert_eq!(0, trailers.len());
+    }
+
+    #[test]
+    fn test_parse_commit_msg_single_line_with_trailer() {
+        let (message, trailers) = GerritChangeIdAction::parse_commit_message(
+            "This is a message\n\nThis is a description\n\nKey: Value",
+        )
+        .unwrap();
+        assert_eq!("This is a message\n\nThis is a description", message);
+        assert_eq!(1, trailers.len());
+        assert_eq!("Value", trailers.get("Key").unwrap());
+    }
+
+    #[test]
+    fn test_parse_commit_msg_single_line_with_trailers() {
+        let (message, trailers) = GerritChangeIdAction::parse_commit_message(
+            "This is a message\n\nThis is a description\n\nKey: Value\nKey2: Value2",
+        )
+        .unwrap();
+        assert_eq!("This is a message\n\nThis is a description", message);
+        assert_eq!(2, trailers.len());
+        assert_eq!("Value", trailers.get("Key").unwrap());
+        assert_eq!("Value2", trailers.get("Key2").unwrap());
+    }
+
+    #[test]
+    fn test_parse_commit_msg_single_line_with_bogus_trailer() {
+        let (message, trailers) = GerritChangeIdAction::parse_commit_message(
+            "This is a message\n\nNot: Trailer\n\nYes: Trailer\n",
+        )
+        .unwrap();
+        assert_eq!("This is a message\n\nNot: Trailer", message);
+        assert_eq!(1, trailers.len());
+        assert_eq!("Trailer", trailers.get("Yes").unwrap());
+    }
+
+    #[test]
+    fn test_parse_commit_msg_title_looks_like_trailer() {
+        let (message, trailers) =
+            GerritChangeIdAction::parse_commit_message("This: Not a trailer\n").unwrap();
+        assert_eq!("This: Not a trailer", message);
+        assert_eq!(0, trailers.len());
+    }
+
+    #[test]
+    fn test_parse_commit_msg_bad_format() {
+        assert!(!GerritChangeIdAction::parse_commit_message("Line1\nLine2").is_ok());
     }
 }
