bb3201c3eff55b17f25457cdeef9707e92b60878
Tomasz Wiszkowski <tomasz.wiszkowski@gmail.com>
2024-11-24 10:36:09 -0800

    Add comments to repo file
    Change-Id: I7155043c16825c9b0ba59768a610f33cf9d0db63

--- src/repo/repo.rs
+++ src/repo/repo.rs
@@ -10,12 +10,17 @@ use super::GitConfigManager;
 use super::GitConfigManagerImpl;
 use super::Item;
 
+/// Represents a Git repository.
 pub struct GitRepo {
     repo: Rc<Repository>,
     config: Box<dyn GitConfigManager>,
 }
 
 impl GitRepo {
+    /// Creates a new instance of `GitRepo`.
+    ///
+    /// # Returns
+    /// A `Result` containing the `GitRepo` instance or an error.
     pub fn new() -> Result<GitRepo> {
         let repo = Rc::new(Repository::open_from_env()?);
         let config = GitConfigManagerImpl::new(&repo)?;
@@ -23,18 +28,34 @@ impl GitRepo {
         Ok(GitRepo { repo, config })
     }
 
+    /// Returns the working directory of the repository.
+    ///
+    /// # Returns
+    /// An `Option` containing a reference to the working directory path.
     pub fn work_dir(&self) -> Option<&Path> {
         self.repo.workdir()
     }
 
+    /// Returns the configuration directory of the repository.
+    ///
+    /// # Returns
+    /// A reference to the configuration directory path.
     pub fn config_dir(&self) -> &Path {
         self.repo.path()
     }
 
+    /// Returns a reference to the configuration manager.
+    ///
+    /// # Returns
+    /// A reference to the `GitConfigManager`.
     pub fn get_config_manager(&self) -> &Box<dyn GitConfigManager> {
         &self.config
     }
 
+    /// Retrieves the items that are staged for a pre-commit.
+    ///
+    /// # Returns
+    /// A `Result` containing a vector of `Item` or an error.
     pub fn get_pre_commit_items(&self) -> Result<Vec<Item>> {
         info!("Querying pre-commit items");
         let Ok(head) = self.repo.head() else {
@@ -83,6 +104,10 @@ impl GitRepo {
         Ok(paths)
     }
 
+    /// Retrieves the items that were committed in the last commit.
+    ///
+    /// # Returns
+    /// A `Result` containing a vector of `Item` or an error.
     pub fn get_post_commit_items(&self) -> Result<Vec<Item>> {
         info!("Querying post-commit items");
         let Ok(head) = self.repo.head() else {
@@ -140,6 +165,10 @@ impl GitRepo {
         Ok(paths)
     }
 
+    /// Retrieves a list of all files in the repository.
+    ///
+    /// # Returns
+    /// A `Result` containing a vector of `Item` or an error.
     pub fn get_list_of_all_files(&self) -> Result<Vec<Item>> {
         let Ok(head) = self.repo.head() else {
             info!("Could not evaluate HEAD - assuming new repository.");
@@ -172,3 +201,4 @@ impl GitRepo {
         Ok(paths)
     }
 }
+
